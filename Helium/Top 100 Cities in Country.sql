d7aae85b156ab1846361a442af31989e2a7d86f9
klaagbaas <64561028+klaagbaas@users.noreply.github.com>
2022-04-24 21:57:23 +0200

    Create Top 100 Cities in Country.sql

---
+++ Helium/Top 100 Cities in Country.sql
@@ -0,0 +1,15 @@
+SELECT SUM(amount * 1e-8) earnings, COUNT(DISTINCT address), long_city city
+FROM gateway_inventory inv
+    LEFT JOIN locations locs ON (inv.location = locs.location)
+    LEFT JOIN rewards
+        ON (inv.address = rewards.gateway)
+WHERE locs.short_country = 'NL'
+    AND rewards.block > (SELECT MIN(height)
+                            FROM blocks
+                            WHERE CAST(TO_TIMESTAMP(time) AS date) BETWEEN CAST((CAST(NOW() AS timestamp) + (INTERVAL '-1 day')) AS date) AND CAST(NOW() AS date))
+                            AND rewards.block < (SELECT MAX(height)
+                            FROM blocks
+                            WHERE CAST(TO_TIMESTAMP(time) AS date) BETWEEN CAST((CAST(NOW() AS timestamp) + (INTERVAL '-1 day')) AS date) AND CAST(NOW() AS date))
+GROUP BY city
+ORDER BY earnings DESC
+LIMIT 100
